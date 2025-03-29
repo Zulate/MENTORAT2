89b243ac5e9d5533d953eea5df71b13b8e57c5b8
Zulate <marco.stalder@stud.hslu.ch>
2025-03-29 17:34:22 +0100

    Added JSON file and implemented Logic to load and display it

--- GODOT-Projects/sprite-anim-test-walk/global-script.gd
+++ GODOT-Projects/sprite-anim-test-walk/global-script.gd
@@ -1,3 +1,7 @@
 extends Node
 
 var player_position
+
+func _ready() -> void:
+	print("First Text: " + StaticData.textData["denialDialogue"]["spirit_dialogue_1"]);
+	print("Second Text: " + StaticData.textData["denialDialogue"]["cube_dialogue_1"]);
---
+++ GODOT-Projects/sprite-anim-test-walk/scripts/staticdata.gd
@@ -0,0 +1,25 @@
+extends Node
+
+var textData = {}
+
+var data_file_path = "res://assets/Textfiles/dialogueFile.JSON"
+
+func _ready():
+	textData = load_json_file(data_file_path)
+
+
+func load_json_file(filePath : String):
+	
+	if FileAccess.file_exists(filePath):
+		
+		var dataFile = FileAccess.open(filePath, FileAccess.READ)
+		var parsedResult = JSON.parse_string(dataFile.get_as_text())
+		
+		if parsedResult is Dictionary:
+			return parsedResult
+			
+		else:
+			print("Error reading File")
+	
+	else:
+		print("File doesnt exist");
